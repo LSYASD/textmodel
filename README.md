78b73cf04c7fd8167a19804874266b24cbe78acc
LSYASD <shiyang.li@wur.nl>
2023-05-01 12:12:15 +0200

    change

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # textmodel
  a soil nitrogen model
+
+ this is try to connect the local library to the remote library
---
+++ text-1.php
@@ -0,0 +1,15 @@
+/**
+ * undocumented function summary
+ *
+ * Undocumented function long description
+ *
+ * @param Type $var Description
+ * @return type
+ * @throws conditon
+ **/
+public function FunctionName(Type $var = null)
+{
+    # code...
+}
+
+another example
